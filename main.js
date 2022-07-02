f285020ee0c1573431f22434ffd6b0a37a4d2062
JediDiegoBM <94797600+JediDiegoBM@users.noreply.github.com>
2022-07-02 10:42:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+noseX = 0
+
+noseY = 0
+
+function preload() {
+clown_nose = loadImage("clownnose.png")
+}
+
+function setup() {
+canvas = createCanvas(300,300);
+canvas.center();
+}
+
+function draw() {
+
+}
